4a14939eb9fa5f9ca2418cbff4c528ea16f2e5f6
Rik <rdewijn@xs4all.nl>
2021-04-29 15:05:19 +0000

    initial version

--- main.R
+++ main.R
@@ -1,9 +1,35 @@
 library(tercen)
 library(dplyr)
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
+do.spline <- function(df, spar, all_knots, n_knots) {
+  m <- smooth.spline(x = df$.x, y = df$.y, spar = spar, all.knots = all_knots, nknots = n_knots)
+  x_fit = df$.x
+  y_fit = fitted(m)
+  y_res = resid(m)
+  df_out <- data.frame(
+    .ri = df$.ri[1],
+    .ci = df$.ci[1],
+    x_fit,
+    y_fit,
+    y_res
+  )
+  return(df_out)
+}
+
+(ctx = tercenCtx())
+
+
+spar = 0.7
+if(!is.null(ctx$op.value('smooth_par'))) spar <- as.numeric(ctx$op.value('smooth_par'))
+all_knots = FALSE
+if(!is.null(ctx$op.value('all_knots'))) all_knots <- as.numeric(ctx$op.value('all_knots'))
+n_knots = 10
+if(!is.null(ctx$op.value('n_knots'))) n_knots <- as.numeric(ctx$op.value('n_knots'))
+
+ctx %>% 
+  select(.ci, .ri, .x, .y) %>%
   group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+  do(do.spline(., spar, all_knots, n_knots)) %>% 
   ctx$addNamespace() %>%
   ctx$save()
+
--- workspace.R
+++ workspace.R
@@ -1,15 +1,11 @@
 library(tercen)
 library(dplyr)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "f8e4f5c67f10ac16716c04576d000eb2")
+options("tercen.stepId"     = "ccda19cf-89c7-4915-b461-2a15b496a8ba")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
-  ctx$addNamespace() %>%
-  ctx$save()
+source("./main.R")
+
